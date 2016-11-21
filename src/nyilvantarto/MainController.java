34e08959b0c0273ba350d2c2368cdff05987de60
szabogabor10 <szabogabor10@hotmail.com>
2016-11-21 16:46:49 +0100

    Haladás

--- src/nyilvantarto/MainController.java
+++ src/nyilvantarto/MainController.java
@@ -5,12 +5,22 @@
  */
 package nyilvantarto;
 
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.ComboBox;
 
 /**
  *
@@ -18,13 +28,9 @@ import javafx.scene.control.TextField;
  */
 public class MainController implements Initializable {
 
+    ObservableList<String> olTermék = FXCollections.observableArrayList();
     @FXML
-    private Label lblHiba;
-    @FXML
-    private TextField txtFelhasznalonev;
-    @FXML
-    private TextField txtJelszo;
-    
+    private ComboBox cbTermék = new ComboBox();
 
     @FXML
     private void kilepes() {
@@ -32,9 +38,36 @@ public class MainController implements Initializable {
         System.exit(0);
     }
 
+    @FXML
+    private void SelectedIndexChanged(ActionEvent e) {
+        if (e.getSource() == cbTermék) {
+            System.out.println(cbTermék.valueProperty().getValue().toString());
+        }
+
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        // TODO code application logic here
+        ArrayList<aru> lista = new ArrayList<>();
+
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(new File("alma.dat")))) {
+            while (true) {
+                lista.add((aru) ois.readObject());
+            }
+        } catch (EOFException e) {
+        } catch (FileNotFoundException e) {
+            System.out.println("Nem találom a fájlt! Hova raktad?!");
+        } catch (IOException e) {
+            System.out.println(e.toString());
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.toString());
+        }
+        Collections.sort(lista);
+        for (aru termék : lista) {
+            olTermék.add(termék.getNev());
+        }
+        cbTermék.setItems(olTermék);
     }
 
 }
--- src/nyilvantarto/teszt.java
+++ src/nyilvantarto/teszt.java
@@ -5,8 +5,15 @@
  */
 package nyilvantarto;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -16,14 +23,51 @@ import java.util.logging.Logger;
  */
 public class teszt {
     public static void main(String[] args) {
-        Felhasznalo emberek = new Felhasznalo("alma", "kisalma", "korte", 0210);
-        Felhasznalo emberek2 = new Felhasznalo("alma", "kisalma", "korte", 0210);
-        try {
-            System.out.println(emberek.validatePassword("kisalma"));
-        } catch (NoSuchAlgorithmException ex) {
-            Logger.getLogger(teszt.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (InvalidKeySpecException ex) {
-            Logger.getLogger(teszt.class.getName()).log(Level.SEVERE, null, ex);
+//        Felhasznalo emberek = new Felhasznalo("alma", "kisalma", "korte", 0210);
+//        Felhasznalo emberek2 = new Felhasznalo("alma", "kisalma", "korte", 0210);
+//        try {
+//            System.out.println(emberek.validatePassword("kisalma"));
+//        } catch (NoSuchAlgorithmException ex) {
+//            Logger.getLogger(teszt.class.getName()).log(Level.SEVERE, null, ex);
+//        } catch (InvalidKeySpecException ex) {
+//            Logger.getLogger(teszt.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        ArrayList <Lista> lista = new ArrayList<>();
+//        lista.add(new Lista("Mizo tej 1 liter "));
+//        lista.add(new Lista("Lada"));
+        ArrayList<aru> lista = new ArrayList<>();
+        lista.add(new aru("körte", "kiló", 300, 20));
+        lista.add(new aru("alma", "kiló", 200, 2));
+        lista.add(new aru("banán", "kiló", 400, 1));
+    
+    
+    File file = new File("alma.dat");
+        FileOutputStream fos = null;
+        ObjectOutputStream oos = null;
+        
+      try {    
+            fos = new FileOutputStream(file);
+            oos = new ObjectOutputStream(fos);
+            for (aru lista1 : lista) {
+                oos.writeObject(lista1);
+              
+          }
+            
+} catch (FileNotFoundException ex) {
+            System.out.println("Lökd ide a fájlt!");
         }
-    }
+        catch (IOException ex) {
+                ex.printStackTrace();
+        }
+      
+      
+        System.out.println(lista);
+        Collections.sort(lista);
+        // 
+        //Collections.s
+        System.out.println(lista);
+        
+         
 }
+    }
+
