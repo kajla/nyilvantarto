6cacc116b09a0331cf13dce138188ddf72d8fb52
szabogabor10 <szabogabor10@hotmail.com>
2016-12-05 20:28:59 +0100

    Log törlés hotfix

--- src/nyilvantarto/MainController.java
+++ src/nyilvantarto/MainController.java
@@ -399,6 +399,7 @@ public class MainController implements Initializable {
         }
         if (e.getSource() == btLogTorles) {
             txLog.clear();
+            nyilvantarto.setLog("");
         }
     }
 
