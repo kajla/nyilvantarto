2bde278c34d2c38da350a9b0658541326c0b17dd
kajla <adamradovits12@hotmail.com>
2016-12-11 18:20:33 +0100

    Felesleges importok eltávolítva

--- src/nyilvantarto/MainController.java
+++ src/nyilvantarto/MainController.java
@@ -21,7 +21,6 @@ import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.geometry.Insets;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
@@ -35,10 +34,6 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 /**
